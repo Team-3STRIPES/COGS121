0349950da2bde1c8dc45f6bc36c9551d1dfb371a
Eunice Chan <starlite625@gmail.com>
2019-05-11 17:43:17 -0700

    js is really hard

--- static_files/js/test.js
+++ static_files/js/test.js
@@ -8,7 +8,8 @@ $(document).ready(() => {
 			a1: 'amazing; spectacular; very good',
 			a2: 'terrible, depressing',
 			a3: 'to light a candle',
-			a4: 'bright; luminescent'
+			a4: 'bright; luminescent',
+			correct: 'a1'
 
 		},
 		{
@@ -16,7 +17,9 @@ $(document).ready(() => {
 			a1: 'burning',
 			a2: 'extremely amazing; fantastic',
 			a3: 'to light a candle',
-			a4: 'bright; luminescent'
+			a4: 'bright; luminescent',
+			correct: 'a2'
+
 
 		},
 		{
@@ -24,7 +27,8 @@ $(document).ready(() => {
 			a1: 'kicking someone backwards',
 			a2: 'terrible, depressing',
 			a3: 'a party',
-			a4: 'bright; luminescent'
+			a4: 'bright; luminescent',
+			correct: 'a3'
 
 		},
 		{
@@ -32,7 +36,9 @@ $(document).ready(() => {
 			a1: 'good',
 			a2: 'alright',
 			a3: 'terrible',
-			a4: 'an herbal medicine'
+			a4: 'an herbal medicine',
+			correct: 'a2'
+
 
 		},
 		{
@@ -40,7 +46,8 @@ $(document).ready(() => {
 			a1: 'good',
 			a2: 'alright',
 			a3: 'terrible',
-			a4: 'stalker fan; passionately obsessed with something'
+			a4: 'stalker fan; passionately obsessed with something',
+			correct: 'a4'
 
 		},
 		{
@@ -48,7 +55,8 @@ $(document).ready(() => {
 			a1: 'good',
 			a2: 'bad',
 			a3: 'perfect; great',
-			a4: 'an herbal medicine'
+			a4: 'an herbal medicine',
+			correct: 'a3'
 
 		},
 		{
@@ -56,7 +64,9 @@ $(document).ready(() => {
 			a1: 'good',
 			a2: 'fixing to',
 			a3: 'perfect; great',
-			a4: 'an herbal medicine'
+			a4: 'an herbal medicine',
+			correct: 'a2'
+
 
 		},
 		{
@@ -64,7 +74,9 @@ $(document).ready(() => {
 			a1: 'good',
 			a2: 'bad',
 			a3: 'extremely; obviously',
-			a4: 'an herbal medicine'
+			a4: 'an herbal medicine',
+			correct: 'a3'
+
 
 		},
 		{
@@ -72,7 +84,9 @@ $(document).ready(() => {
 			a1: 'good',
 			a2: 'bad',
 			a3: 'discretely; not obviously',
-			a4: 'an herbal medicine'
+			a4: 'an herbal medicine',
+			correct: 'a3'
+
 
 		},
 		{
@@ -80,20 +94,42 @@ $(document).ready(() => {
 			a1: 'good',
 			a2: 'bad',
 			a3: 'perfect; great',
-			a4: 'family'
+			a4: 'family',
+			correct: 'a4'
+
 
 		},
 	];
 
 	function setQuestions() {
-    $('#question').text(dummyQuestions[currentIndex].question);
-    $('#a1').text(dummyQuestions[currentIndex].a1);
-    $('#a2').text(dummyQuestions[currentIndex].a2);
-    $('#a3').text(dummyQuestions[currentIndex].a3);
-    $('#a4').text(dummyQuestions[currentIndex].a4);
-  }
+	    $('#question').text(dummyQuestions[currentIndex].question);
+	    $('#a1').text(dummyQuestions[currentIndex].a1);
+	    $('#a2').text(dummyQuestions[currentIndex].a2);
+	    $('#a3').text(dummyQuestions[currentIndex].a3);
+	    $('#a4').text(dummyQuestions[currentIndex].a4);
+  	}
 
-  setQuestions();
-  console.log("hello");
+  	setQuestions();
+
+  	// marks selected as yellow, not selected as gray
+
+  	$('.answer-option').on('click', function() {
+  		$('.answer-option').css("background-color", "#EEE");
+  		$(this).css("background-color", "#f7d345");
+
+  	})
+
+  	$('.submitbtn').on('click', (e) => {
+  		e.preventDefault();
+  		if (currentIndex === 9) {
+  			alert("You friccin moron.");
+  		}
+		else {
+			currentIndex++;
+			setQuestions();
+		}  		
+  	})
+
+
 	
 });
