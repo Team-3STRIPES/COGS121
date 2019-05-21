2484986176512e6ea3977745ae5a5268a214e9df
Nathaniel Qin <nathaniel.qin1@gmail.com>
2019-05-21 15:06:23 -0700

    fixed some minor typos

--- static_files/js/login.js
+++ static_files/js/login.js
@@ -9,16 +9,12 @@ $(document).ready(() => {
       var token = result.credential.accessToken;
       // The signed-in user info.
       var user = result.user;
-      // ...
+      
+
     }).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
     });
   });
 
